658cf690f3b430efd555ec2f550101378b1500ec
Buor <buor0@mail.ru>
2021-08-09 14:42:57 +0300

    Started styling content on main page

--- client/src/Components/Main/MainPage/MainContent.tsx
+++ client/src/Components/Main/MainPage/MainContent.tsx
@@ -1,8 +1,177 @@
+import { NavLink } from 'react-router-dom'
+
 const MainContent: React.FC = () => {
 
     return (
         <div className={'main_content'}>
+            <div className={'main_section'}>
+                <div className='title'>Some title</div>
+                <div className='items_wrapper'>
+                    <div className='item'>
+                        <div className='item_head'>
 
+                        </div>
+                        <NavLink to={'/'}>
+                            <img className={'item_image'} src='https://clipart-best.com/img/tea/tea-clip-art-14.png'
+                                 alt='tea' />
+                        </NavLink>
+                        <NavLink to={'/'}>
+                            <div className='item_name'>Some item</div>
+                        </NavLink>
+
+                        <div className='item_discount'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+                        <div className='item_cost_wrapper'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+
+                        <div className='item_stock'>Нет в наличии</div>
+                    </div>
+                    <div className='item'>
+                        <div className='item_head'>
+
+                        </div>
+                        <NavLink to={'/'}>
+                            <img className={'item_image'} src='https://clipart-best.com/img/tea/tea-clip-art-14.png'
+                                 alt='tea' />
+                        </NavLink>
+                        <NavLink to={'/'}>
+                            <div className='item_name'>Some item</div>
+                        </NavLink>
+
+                        <div className='item_discount'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+                        <div className='item_cost_wrapper'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+
+                        <div className='item_stock'>Нет в наличии</div>
+                    </div>
+                    <div className='item'>
+                        <div className='item_head'>
+
+                        </div>
+                        <NavLink to={'/'}>
+                            <img className={'item_image'} src='https://clipart-best.com/img/tea/tea-clip-art-14.png'
+                                 alt='tea' />
+                        </NavLink>
+                        <NavLink to={'/'}>
+                            <div className='item_name'>Some item</div>
+                        </NavLink>
+
+                        <div className='item_discount'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+                        <div className='item_cost_wrapper'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+
+                        <div className='item_stock'>Нет в наличии</div>
+                    </div>
+                    <div className='item'>
+                        <div className='item_head'>
+
+                        </div>
+                        <NavLink to={'/'}>
+                            <img className={'item_image'} src='https://clipart-best.com/img/tea/tea-clip-art-14.png'
+                                 alt='tea' />
+                        </NavLink>
+                        <NavLink to={'/'}>
+                            <div className='item_name'>Some item</div>
+                        </NavLink>
+
+                        <div className='item_discount'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+                        <div className='item_cost_wrapper'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+
+                        <div className='item_stock'>Нет в наличии</div>
+                    </div>
+                    <div className='item'>
+                        <div className='item_head'>
+
+                        </div>
+                        <NavLink to={'/'}>
+                            <img className={'item_image'} src='https://clipart-best.com/img/tea/tea-clip-art-14.png'
+                                 alt='tea' />
+                        </NavLink>
+                        <NavLink to={'/'}>
+                            <div className='item_name'>Some item</div>
+                        </NavLink>
+
+                        <div className='item_discount'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+                        <div className='item_cost_wrapper'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+
+                        <div className='item_stock'>Нет в наличии</div>
+                    </div>
+                    <div className='item'>
+                        <div className='item_head'>
+
+                        </div>
+                        <NavLink to={'/'}>
+                            <img className={'item_image'} src='https://clipart-best.com/img/tea/tea-clip-art-14.png'
+                                 alt='tea' />
+                        </NavLink>
+                        <NavLink to={'/'}>
+                            <div className='item_name'>Some item</div>
+                        </NavLink>
+
+                        <div className='item_discount'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+                        <div className='item_cost_wrapper'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+
+                        <div className='item_stock'>Нет в наличии</div>
+                    </div>
+                    <div className='item'>
+                        <div className='item_head'>
+
+                        </div>
+                        <NavLink to={'/'}>
+                            <img className={'item_image'} src='https://clipart-best.com/img/tea/tea-clip-art-14.png'
+                                 alt='tea' />
+                        </NavLink>
+                        <NavLink to={'/'}>
+                            <div className='item_name'>Some item</div>
+                        </NavLink>
+
+                        <div className='item_discount'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+                        <div className='item_cost_wrapper'>
+                            <span className='item_cost'>110</span>
+                            <span className='item_currency'>$</span>
+                        </div>
+
+                        <div className='item_stock'>Нет в наличии</div>
+                    </div>
+
+
+                </div>
+            </div>
         </div>
     )
 }
