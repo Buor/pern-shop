ea639b35e1156fafb14057cb67ec2236b80c9234
Buor <buor0@mail.ru>
2021-08-22 16:56:27 +0300

    feat(client/categoryPage): added discountCost display

--- client/src/Components/Categories/Sections/Content/ContentProduct.tsx
+++ client/src/Components/Categories/Sections/Content/ContentProduct.tsx
@@ -6,7 +6,7 @@ interface Props {
     product: CategoryProductDTO
 }
 
-const ContentProduct: React.FC<Props> = ({product: {img,cost,discountCost,name,count,typePropertyValues}}) => {
+const ContentProduct: React.FC<Props> = ({ product: { img, cost, discountCost, name, count, typePropertyValues } }) => {
     return (
         <div className='product'>
             <div className='head'></div>
@@ -18,7 +18,17 @@ const ContentProduct: React.FC<Props> = ({product: {img,cost,discountCost,name,c
             <div className='name'>
                 {name}
             </div>
-            <div className='price'>{cost} $</div>
+            {
+                discountCost
+                    ? (
+                        <>
+                            <div className={'discount_price'}>{cost} $</div>
+                            <div className='price'>{discountCost} $</div>
+                        </>
+                    )
+                    : <div className='price'>{cost} $</div>
+            }
+
         </div>
     )
 }
