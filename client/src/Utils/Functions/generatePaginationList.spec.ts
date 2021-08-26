b8e6e620e2b718efdcfa2ad368335ba8a9204d1b
Buor <buor0@mail.ru>
2021-08-26 16:24:27 +0300

    test(client/categoryPage/pagination): setup tests for generatePaginationList function

--- client/src/Utils/Functions/generatePaginationList.spec.ts
+++ client/src/Utils/Functions/generatePaginationList.spec.ts
@@ -1,6 +1,18 @@
-//@ts-ignore
 import { generatePaginationList } from './generatePaginationList'
-
-it('should equal [1,2,3,4,5,6,7,8,9]', () => {
-    expect(generatePaginationList(255,2).toString()).toBe([1,2,3,4,5,6,7,8,9].toString())
+describe("Testing generatePaginationList function", () => {
+    it('start check', () => {
+        expect(generatePaginationList(1024,2).toString()).toBe([1,2,3,4,5,6,7,8,9].toString())
+    })
+    it('middle check 1', () => {
+        expect(generatePaginationList(1024,13).toString()).toBe([9,10,11,12,13,14,15,16,17].toString())
+    })
+    it('middle check 2', () => {
+        expect(generatePaginationList(2015,8).toString()).toBe([4,5,6,7,8,9,10,11,12].toString())
+    })
+    it('few pages check', () => {
+        expect(generatePaginationList(107,3).toString()).toBe([1,2,3].toString())
+    })
+    it('empty check', () => {
+        expect(generatePaginationList(21,1).toString()).toBe([].toString())
+    })
 })
--- client/src/Utils/Functions/generatePaginationList.ts
+++ client/src/Utils/Functions/generatePaginationList.ts
@@ -1,3 +1,3 @@
 export const generatePaginationList = (productsCount: number,currentPage: number = 1): number[] => {
-    return [1,2,3,4,5,6,7,8,9]
+    return []
 }
