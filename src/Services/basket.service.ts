9a8c195f57db357ebdd424f0ab304f591e3fb505
Buor <buor0@mail.ru>
2021-08-27 14:46:55 +0300

    fix(server/basket/addProductToBasket): if product already exists basket, it'll not get there second time

--- src/Services/basket.service.ts
+++ src/Services/basket.service.ts
@@ -6,21 +6,21 @@ import Basket from '../Entities/Basket'
 
 @Injectable()
 export class BasketService {
-    async addProductToBasket({productId,userId} :AddProductToBasketDTO): Promise<boolean> {
+    async addProductToBasket({ productId, userId }: AddProductToBasketDTO): Promise<boolean> {
         const user = await User.findOne(userId)
-        if(!user)
-            throw new HttpException(`User with id ${userId} doesn't exist!`,400)
+        if (!user)
+            throw new HttpException(`User with id ${userId} doesn't exist!`, 400)
 
         const product = await Product.findOne(productId)
-        if(!product)
-            throw new HttpException(`Product with id ${productId} doesn't exist!`,400)
+        if (!product)
+            throw new HttpException(`Product with id ${productId} doesn't exist!`, 400)
 
         const basket = await Basket.findOne(user.basket.id)
-        if(!basket)
-            throw new HttpException(`User with id ${user.id} doesn't have basket!`,400)
+        if (!basket)
+            throw new HttpException(`User with id ${user.id} doesn't have basket!`, 400)
 
-        if(!basket.products)
-            basket.products = []
+        if (basket.products.find(prod => prod.id === product.id))
+            return true
 
         basket.products.push(product)
         await Basket.save(basket)
