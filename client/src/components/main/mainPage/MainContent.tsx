1ed7b4ecb6a5342b00925d417c3adaec47fed79a
Buor <buor0@mail.ru>
2021-09-19 21:47:57 +0300

    feat(client/mainPage): started MainPage rework
    * Removed ProductItem component
    * Removed SectionAll component
    * Added describing HTML to MainContent with titles and paragraphs
    * Added a few cards with cards wrapper in MainContent

--- client/src/components/main/mainPage/MainContent.tsx
+++ client/src/components/main/mainPage/MainContent.tsx
@@ -1,11 +1,36 @@
-import { SectionAll } from './contentSections/SectionAll'
+import React from 'react'
+import { Card } from '../../../utils/uiComponents/Card'
 
 const MainContent: React.FC = () => {
 
     return (
         <div className={'main_content'}>
-            <div className={'main_section'}>
-                <SectionAll/>
+            <h1>Welcome to Tea Shop!</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, beatae cupiditate delectus dicta dolor,
+                doloremque eligendi enim fugiat incidunt minus mollitia nam nulla officia quae qui, rerum sequi. Ab
+                architecto eius fugiat harum perspiciatis sit. Ab alias aliquam cum delectus dolores facere laborum
+                molestias, necessitatibus provident, quo quos ratione veniam.
+            </p>
+            <h2>Check out best <span>categories!</span></h2>
+            <div className='cards_wrapper'>
+                <Card title='Title'
+                      description='Desc'
+                      backgroundUrl='https://avatars.mds.yandex.net/get-zen_doc/1895194/pub_5ea08c7a88edb84e60bfcce5_5ea0901e6647a20c331969ea/scale_1200'
+                      textColor='white'
+                      onClick={() => {console.log('click')}}
+                />
+                <Card title='Title'
+                      description='Desc'
+                      backgroundUrl='https://avatars.mds.yandex.net/get-zen_doc/1895194/pub_5ea08c7a88edb84e60bfcce5_5ea0901e6647a20c331969ea/scale_1200'
+                      textColor='white'
+                      onClick={() => {console.log('click')}}
+                />
+                <Card title='Title'
+                      description='Desc'
+                      backgroundUrl='https://avatars.mds.yandex.net/get-zen_doc/1895194/pub_5ea08c7a88edb84e60bfcce5_5ea0901e6647a20c331969ea/scale_1200'
+                      textColor='white'
+                      onClick={() => {console.log('click')}}
+                />
             </div>
         </div>
     )
