cc25b8f5cefbccd5c3513bd69aa20f6f183fa5a2
Buor <buor0@mail.ru>
2021-09-20 09:44:16 +0300

    feat(client/mainPage): added description and onClick function to ui cards
    * OnClick card functions redirect to appropriate category page
    * Added backgrounds and changed titles and descriptions on cards

--- client/src/components/main/mainPage/MainContent.tsx
+++ client/src/components/main/mainPage/MainContent.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Card } from '../../../utils/uiComponents/Card'
+import { useHistory } from 'react-router-dom'
 
 const MainContent: React.FC = () => {
+
+    const history = useHistory()
+
+    const redirectToCategory = (categoryName: string) => {
+        history.push(`/category/${categoryName}`)
+    }
 
     return (
         <div className={'main_content'}>
@@ -13,23 +20,23 @@ const MainContent: React.FC = () => {
             </p>
             <h2>Check out best <span>categories!</span></h2>
             <div className='cards_wrapper'>
-                <Card title='Title'
-                      description='Desc'
+                <Card title='Tea'
+                      description='Tea is a main thing of out shop!'
                       backgroundUrl='https://avatars.mds.yandex.net/get-zen_doc/1895194/pub_5ea08c7a88edb84e60bfcce5_5ea0901e6647a20c331969ea/scale_1200'
                       textColor='white'
-                      onClick={() => {console.log('click')}}
+                      onClick={() => redirectToCategory('tea')}
                 />
-                <Card title='Title'
-                      description='Desc'
-                      backgroundUrl='https://avatars.mds.yandex.net/get-zen_doc/1895194/pub_5ea08c7a88edb84e60bfcce5_5ea0901e6647a20c331969ea/scale_1200'
+                <Card title='Coffee'
+                      description='The aroma of fine coffee will help you in any situation!'
+                      backgroundUrl='https://phonoteka.org/uploads/posts/2021-05/1621032878_6-phonoteka_org-p-kofe-zernovoi-fon-12.jpg'
                       textColor='white'
-                      onClick={() => {console.log('click')}}
+                      onClick={() => redirectToCategory('coffee')}
                 />
-                <Card title='Title'
-                      description='Desc'
-                      backgroundUrl='https://avatars.mds.yandex.net/get-zen_doc/1895194/pub_5ea08c7a88edb84e60bfcce5_5ea0901e6647a20c331969ea/scale_1200'
+                <Card title='Chicory'
+                      description='Chicory does not contain caffeine at all, and is not inferior in quality to coffee!'
+                      backgroundUrl='https://img5.goodfon.com/original/2560x1440/b/b7/tsvetok-tsikorii-goluboi-tsvet.jpg'
                       textColor='white'
-                      onClick={() => {console.log('click')}}
+                      onClick={() => redirectToCategory('chicory')}
                 />
             </div>
         </div>
