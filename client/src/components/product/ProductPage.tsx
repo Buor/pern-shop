86cc3bd67d0379f4a0813a7ca5cd904d9bfc294b
Buor <buor0@mail.ru>
2021-09-23 11:37:25 +0300

    fix(client/productPage): active product tab not highlighting fix

--- client/src/components/product/ProductPage.tsx
+++ client/src/components/product/ProductPage.tsx
@@ -33,8 +33,12 @@ export const ProductPage: React.FC<IProps> = () => {
             <div className={styles.title + ' container'}>{product.name}</div>
             <nav className={styles.navWrapper}>
                 <div className={styles.navigation + ' container'}>
-                    <button className={styles.active} onClick={() => setCurrentSection('All about Product')}>Все о товаре</button>
-                    <button onClick={() => setCurrentSection('Stats')}>Характеристики</button>
+                    <button className={currentSection === 'All about Product' ? styles.active : ''}
+                            onClick={() => setCurrentSection('All about Product')}>Все о товаре
+                    </button>
+                    <button className={currentSection === 'Stats' ? styles.active : ''}
+                            onClick={() => setCurrentSection('Stats')}>Характеристики
+                    </button>
                 </div>
             </nav>
             <div className='container'>
